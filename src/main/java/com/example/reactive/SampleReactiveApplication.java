7dcd83000b80f8f00e479bc4cb58d63b078a4f7a
chocolaolantan <chocolaolantan@gmail.com>
2019-02-20 21:58:29 +0900

    firstcommit

---
+++ src/main/java/com/example/reactive/SampleReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleReactiveApplication.class, args);
+	}
+
+}
